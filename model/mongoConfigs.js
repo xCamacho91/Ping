1827377adaa343d6d9320e62ab6dbf62cbffff65
Maurício Camacho <95317063+xCamacho91@users.noreply.github.com>
2022-02-13 21:36:37 +0000

    Update mongoConfigs.js

--- model/mongoConfigs.js
+++ model/mongoConfigs.js
@@ -4,7 +4,7 @@ var db;
 
 module.exports = {
     connect: function (callback) {
-        MongoClient.connect('mongodb+srv://G15:uUKLmrjopIJ46eQZ@clusterdbw.1dbjr.mongodb.net/G15?authSource=admin&replicaSet=atlas-bek8xj-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', { useUnifiedTopology: true },function (err, database) {
+        MongoClient.connect('URl for mongoDB connection', { useUnifiedTopology: true },function (err, database) {
             console.log('Connected the database on port 27017');
             db = database.db('G15');
             callback(err);
